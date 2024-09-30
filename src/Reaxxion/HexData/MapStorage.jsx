fbac54b78f896930728a813f4d56fa0c64876ae4
Will O'Brien <wno9619@g.rit.edu>
2024-09-30 00:39:48 -0400

    Hexagon and Map Storage Data Structures (Incomplete)

---
+++ src/Reaxxion/HexData/MapStorage.jsx
@@ -0,0 +1,14 @@
+import { Hex } from "./Hexagons";
+
+export class MapStorage {
+    // Constructor
+    constructor() {
+        
+        hashmap = new Map();
+    }
+    addHex(hex) {
+        hashmap.set({ q: hex.q, r: hex.r}, );
+    }
+
+
+}
