ea8c0a5893cf3c8b7dae6f693f3ddec8411ef755
b-book <usefulinternetmailbox@gmail.com>
2024-09-30 20:21:25 -0400

    MapStorage Refactor (breaks some stuff)

--- src/Reaxxion/HexData/MapStorage.jsx
+++ src/Reaxxion/HexData/MapStorage.jsx
@@ -3,17 +3,11 @@ import { Hex, Layout } from "./HexMath";
 export class MapStorage {
     // Constructor
     constructor(orientation, size, origin) {
-        this.hexArray = [];
         this.hexLayout = new Layout(orientation, size, origin);
         this.hexHash = new Map();
     }
-    addHex(hex) {
-        const i = this.hexArray.push(hex);
-        this.hexHash.set({ q: hex.q, r: hex.r}, i);
-    }
-    setHex(i, hex) {
-        this.hexArray[i] = hex;
-        this.hexHash.set({q: hex.q, r: hex.r}, i);
+    setHex(q, r, s) {
+        this.hexHash.set({q: q, r: r}, s);
     }
     getHex(q, r) {
         return this.hexHash[{q: q, r: r}];
