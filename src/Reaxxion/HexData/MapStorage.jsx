d5f85ce235c9be60b1fc204f4221358663f3b0fe
davidbickley <86498138+davidbickley@users.noreply.github.com>
2024-10-01 17:40:34 -0500

    Dropped in HexData folder from other branch

---
+++ src/Reaxxion/HexData/MapStorage.jsx
@@ -0,0 +1,22 @@
+import { Layout } from "./HexMath";
+
+export class MapStorage {
+    // Constructor
+    constructor(orientation, size, origin) {
+        this.hexLayout = new Layout(orientation, size, origin);
+        this.hexHash = new Map();
+    }
+    setHex(q, r, s) {
+        this.hexHash.set({ q: q, r: r }, s);
+    }
+    getHex(q, r) {
+        return this.hexHash[{ q: q, r: r }];
+    }
+    createMap(boardSize) {
+        for (let q = 0; q < boardSize.height; q++) {
+            for (let r = 0; r < boardSize.width; r++) {
+                this.setHex(q, r, -q - r)
+            }
+        }
+    }
+}
